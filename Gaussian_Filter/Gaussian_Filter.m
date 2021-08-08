e0180da9dd20b5ee25ffba05a00914fcd664a971
alejandrohsanchez <52898001+alejandrohsanchez@users.noreply.github.com>
2021-08-08 02:24:20 -0700

    Uploaded a Gaussian Filtering project
    For more information please refer to the README file.

---
+++ Gaussian_Filter/Gaussian_Filter.m
@@ -0,0 +1,30 @@
+clc
+clear all
+close all
+
+I = imread('BellevueLot.jpg');
+h = [1, 2, 1; 2, 4, 2; 1, 2, 1];
+weight = 1/16;
+h = weight * h;
+
+R = I(:, :, 1);
+G = I(:, :, 2);
+B = I(:, :, 3);
+
+Red_filter = imfilter(R, h);
+Green_filter = imfilter(G, h);
+Blue_filter = imfilter(B, h);
+
+combined = cat(3, R, G, B);
+
+figure;
+imshow(Red_filter);
+
+figure;
+imshow(Green_filter);
+
+figure;
+imshow(Blue_filter);
+
+figure;
+imshow(combined)
---
+++ Gaussian_Filter/README.txt
@@ -0,0 +1,5 @@
+This MATLAB script applies a standard 3x3 Gaussian filter on an image's RGB channels independently. The MATLAB script uses the image processing toolbox and the imread function in the module. The Gaussian kernel is the following:
+
+1, 2, 1; 2, 4, 2; 1, 2, 1
+
+The weight of the kernel multiplier is 1/16.
