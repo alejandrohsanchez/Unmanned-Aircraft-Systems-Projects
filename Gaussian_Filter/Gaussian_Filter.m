378e9de22df704564a41eb2b9d877c2419cb33d8
alejandrohsanchez <52898001+alejandrohsanchez@users.noreply.github.com>
2021-08-10 20:49:19 -0700

    Added some spaces, formatted more lines
    This is a test.

--- Gaussian_Filter/Gaussian_Filter.m
+++ Gaussian_Filter/Gaussian_Filter.m
@@ -3,14 +3,13 @@ clear all
 close all
 
 I = imread('BellevueLot.jpg');
+
 h = [1, 2, 1; 2, 4, 2; 1, 2, 1];
 weight = 1/16;
 h = weight * h;
-
 R = I(:, :, 1);
 G = I(:, :, 2);
 B = I(:, :, 3);
-
 Red_filter = imfilter(R, h);
 Green_filter = imfilter(G, h);
 Blue_filter = imfilter(B, h);
